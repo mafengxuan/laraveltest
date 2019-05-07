a1640d8d58fd47bba6110fc90de21de65cd1c882
mafengxuan <mafengxuan@126.com>
2019-05-07 23:58:48 +0800

    ++

---
+++ app/Model/Money.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Money
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Money newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Money newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Money query()
+ * @mixin \Eloquent
+ */
+class Money extends Model
+{
+    //
+
+}
