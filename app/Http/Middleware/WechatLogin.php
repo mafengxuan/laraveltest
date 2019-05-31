4cff95afdcc920501d5d6aa22776c87553a4c5bc
mafengxuan <mafengxuan@126.com>
2019-05-31 21:30:28 +0800

    ++

--- app/Http/Middleware/WechatLogin.php
+++ app/Http/Middleware/WechatLogin.php
@@ -41,33 +41,42 @@ class WechatLogin
           ]
         }
          */
-        if(!empty(session('wechat.oauth_user.default')) && empty(session('openId'))){
-            $openInfo = session('wechat.oauth_user.default');
-            $openId = $openInfo['original']['openid'];
-            $userInfo = UserInfo::where('openId',$openId)->first();
-            if(!empty($userInfo)){
-                $request->session()->put('userId',$userInfo['userId']);
-                $request->session()->put('openId',$userInfo['openId']);
-                $request->session()->put('nickName',$userInfo['nickName']);
-                $request->session()->put('headimgurl',$userInfo['imgUrl']);
-                $request->session()->put('mobile',$userInfo['mobile']);
-            }else{
-                $originalInfo = $openInfo['original'];
-                $users = new UserInfo;
-                $users->openId = $originalInfo['openid'];
-                $users->nickName = $originalInfo['nickname'];
-                $users->sex = $originalInfo['sex'];
-                $users->province = $originalInfo['province'];
-                $users->city = $originalInfo['city'];
-                $users->imgUrl = $originalInfo['headimgurl'];
-                $users->save();
-                $userInfo = UserInfo::where('openId',$openId)->first();
-                $request->session()->put('userId',$userInfo['userId']);
-                $request->session()->put('openId',$userInfo['openId']);
-                $request->session()->put('nickName',$userInfo['nickName']);
-                $request->session()->put('headimgurl',$userInfo['imgUrl']);
-                $request->session()->put('mobile',$userInfo['mobile']);
-            }
+//        if(!empty(session('wechat.oauth_user.default')) && empty(session('openId'))){
+//            $openInfo = session('wechat.oauth_user.default');
+//            $openId = $openInfo['original']['openid'];
+//            $userInfo = UserInfo::where('openId',$openId)->first();
+//            if(!empty($userInfo)){
+//                $request->session()->put('userId',$userInfo['userId']);
+//                $request->session()->put('openId',$userInfo['openId']);
+//                $request->session()->put('nickName',$userInfo['nickName']);
+//                $request->session()->put('headimgurl',$userInfo['imgUrl']);
+//                $request->session()->put('mobile',$userInfo['mobile']);
+//            }else{
+//                $originalInfo = $openInfo['original'];
+//                $users = new UserInfo;
+//                $users->openId = $originalInfo['openid'];
+//                $users->nickName = $originalInfo['nickname'];
+//                $users->sex = $originalInfo['sex'];
+//                $users->province = $originalInfo['province'];
+//                $users->city = $originalInfo['city'];
+//                $users->imgUrl = $originalInfo['headimgurl'];
+//                $users->save();
+//                $userInfo = UserInfo::where('openId',$openId)->first();
+//                $request->session()->put('userId',$userInfo['userId']);
+//                $request->session()->put('openId',$userInfo['openId']);
+//                $request->session()->put('nickName',$userInfo['nickName']);
+//                $request->session()->put('headimgurl',$userInfo['imgUrl']);
+//                $request->session()->put('mobile',$userInfo['mobile']);
+//            }
+//        }
+
+        if(empty(session('userId'))){
+            $userInfo = UserInfo::find(1);
+            $request->session()->put('userId',$userInfo['userId']);
+            $request->session()->put('openId',$userInfo['openId']);
+            $request->session()->put('nickName',$userInfo['nickName']);
+            $request->session()->put('headimgurl',$userInfo['imgUrl']);
+            $request->session()->put('mobile',$userInfo['mobile']);
         }
 
         return $next($request);
