13b53f203b86761df71188391e093764bc7b81be
lichenglong <554675188@qq.com>
2019-06-10 00:12:57 +0800

    ++

--- admin/src/views/diary/audit/index.vue
+++ admin/src/views/diary/audit/index.vue
@@ -1,5 +1,18 @@
 <template lang="html">
   <div class="app-container">
+    <div class="block" style="margin-bottom:20px;">
+      <div class="el-input" style="float:left;width:180px;margin-right:20px;">
+        <el-input v-model="input" placeholder="微信昵称"></el-input>
+      </div>
+      <el-date-picker
+        v-model="value6"
+        type="daterange"
+        range-separator="至"
+        start-placeholder="开始日期"
+        end-placeholder="结束日期">
+      </el-date-picker>
+      <el-button type="primary" style="margin-left:20px;">筛选</el-button>
+    </div>
     <el-table v-loading="listLoading" :data="list" border style="width: 100%">
       <el-table-column prop="id" label="序号" sortable></el-table-column>
       <el-table-column prop="user.nickName" label="微信昵称"></el-table-column>
@@ -29,7 +42,8 @@ import store from '../../../store'
 export default {
   data() {
     return {
-      listLoading: true
+      listLoading: true,
+      input:''
     }
   },
   computed: {
