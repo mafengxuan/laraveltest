aef1952ee0c879278aab63c1830bad6c881fc814
mafengxuan <mafengxuan@126.com>
2019-06-23 11:03:01 +0800

    ++

--- app/Http/Controllers/Index/MessageController.php
+++ app/Http/Controllers/Index/MessageController.php
@@ -63,15 +63,8 @@ class MessageController extends Controller
 
     public function aboutMe(){
         $userId = session('userId');
-        $articleIds = Article::where('userId',$userId)->get(['id'])->toArray();
-        $ids = [];
-        foreach ($articleIds as $k => $v){
-            $ids[] = $v['id'];
-        }
-        $comments = Comment::whereIn('articleId',$ids)->orderBy('created_at','desc')->with('article')->with('user')->with('reply')->get()->toArray();
-//        $replys = Reply::where('reUserId',$userId)->orderBy('created_at','desc')->with('comment')->get()->toArray();
-//        $result = array_merge($comments,$replys);
-//        array_multisort(array_column($result,'created_at'),SORT_DESC,$result);
+        $articleId = Article::where('userId',$userId)->first();
+        $comments = Comment::where('articleId',$articleId)->orderBy('created_at','desc')->with('article')->with('user')->with('reply')->get()->toArray();
         return response()->json(Result::ok($comments));
     }
     /**
