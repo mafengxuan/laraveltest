ddd2a638aecd7240a254e33749cfee6da193419b
mafengxuan <mafengxuan@126.com>
2019-06-23 10:50:00 +0800

    +6+

--- app/Http/Controllers/Index/MessageController.php
+++ app/Http/Controllers/Index/MessageController.php
@@ -68,11 +68,11 @@ class MessageController extends Controller
         foreach ($articleIds as $k => $v){
             $ids[] = $v['id'];
         }
-        $comments = Comment::whereIn('articleId',$ids)->orderBy('created_at','desc')->with('article')->with('user')->get()->toArray();
-        $replys = Reply::where('reUserId',$userId)->orderBy('created_at','desc')->with('comment')->get()->toArray();
-        $result = array_merge($comments,$replys);
-        array_multisort(array_column($result,'created_at'),SORT_DESC,$result);
-        return response()->json(Result::ok($result));
+        $comments = Comment::whereIn('articleId',$ids)->orderBy('created_at','desc')->with('article')->with('user')->with('reply')->get()->toArray();
+//        $replys = Reply::where('reUserId',$userId)->orderBy('created_at','desc')->with('comment')->get()->toArray();
+//        $result = array_merge($comments,$replys);
+//        array_multisort(array_column($result,'created_at'),SORT_DESC,$result);
+        return response()->json(Result::ok($comments));
     }
     /**
      * Show the form for editing the specified resource.
