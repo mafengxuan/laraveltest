ed5e5dcd6603cbffbfb9ddf8ed2a3a72440ca9b8
mafengxuan <mafengxuan@126.com>
2019-06-23 10:16:21 +0800

    ++

--- app/Http/Controllers/Index/MessageController.php
+++ app/Http/Controllers/Index/MessageController.php
@@ -68,7 +68,7 @@ class MessageController extends Controller
         foreach ($articleIds as $k => $v){
             $ids[] = $v['id'];
         }
-        $comments = Comment::whereIn('articleId',$ids)->orderBy('created_at','desc')->with('article')->get()->toArray();
+        $comments = Comment::whereIn('articleId',$ids)->orderBy('created_at','desc')->with('article')->with('user')->get()->toArray();
         $replys = Reply::where('reUserId',$userId)->orderBy('created_at','desc')->with('comment')->get()->toArray();
         $result = array_merge($comments,$replys);
         array_multisort(array_column($result,'created_at'),SORT_DESC,$result);
