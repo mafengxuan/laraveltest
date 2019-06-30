35c33869f7285d740af726bac59f6a2bb0402b5d
mafengxuan <mafengxuan@126.com>
2019-06-30 22:53:25 +0800

    ++

--- app/Http/Controllers/Wechat/IndexController.php
+++ app/Http/Controllers/Wechat/IndexController.php
@@ -11,38 +11,38 @@ class IndexController extends Controller {
 
 
     public function index(Request $request){
-//        if(!empty(session('wechat.oauth_user.default')) && empty(session('openId'))){
-//            $openInfo = session('wechat.oauth_user.default');
-//            $openId = $openInfo['original']['openid'];
-//            $userInfo = UserInfo::where('openId',$openId)->first();
-//            if(!empty($userInfo)){
-//                $request->session()->put('userId',$userInfo['userId']);
-//                $request->session()->put('openId',$userInfo['openId']);
-//                $request->session()->put('nickname',$userInfo['nickname']);
-//                $request->session()->put('headimgurl',$userInfo['imgUrl']);
-//                $request->session()->put('mobile',$userInfo['mobile']);
-//            }else{
-//                $originalInfo = $openInfo['original'];
-//                $users = new UserInfo;
-//                $users->openId = $originalInfo['openid'];
-//                $users->nickname = $originalInfo['nickname'];
-//                    if($originalInfo['sex'] == 1){
-//                        $users->sex = '男';
-//                    }else{
-//                        $users->sex = '女';
-//                    }
-//                $users->province = $originalInfo['province'];
-//                $users->city = $originalInfo['city'];
-//                $users->imgUrl = $originalInfo['headimgurl'];
-//                $users->save();
-//                $userInfo = UserInfo::where('openId',$openId)->first();
-//                $request->session()->put('userId',$userInfo['userId']);
-//                $request->session()->put('openId',$userInfo['openId']);
-//                $request->session()->put('nickname',$userInfo['nickname']);
-//                $request->session()->put('headimgurl',$userInfo['imgUrl']);
-//                $request->session()->put('mobile',$userInfo['mobile']);
-//            }
-//        }
+        if(!empty(session('wechat.oauth_user.default')) && empty(session('openId'))){
+            $openInfo = session('wechat.oauth_user.default');
+            $openId = $openInfo['original']['openid'];
+            $userInfo = UserInfo::where('openId',$openId)->first();
+            if(!empty($userInfo)){
+                $request->session()->put('userId',$userInfo['userId']);
+                $request->session()->put('openId',$userInfo['openId']);
+                $request->session()->put('nickname',$userInfo['nickname']);
+                $request->session()->put('headimgurl',$userInfo['imgUrl']);
+                $request->session()->put('mobile',$userInfo['mobile']);
+            }else{
+                $originalInfo = $openInfo['original'];
+                $users = new UserInfo;
+                $users->openId = $originalInfo['openid'];
+                $users->nickname = $originalInfo['nickname'];
+                    if($originalInfo['sex'] == 1){
+                        $users->sex = '男';
+                    }else{
+                        $users->sex = '女';
+                    }
+                $users->province = $originalInfo['province'];
+                $users->city = $originalInfo['city'];
+                $users->imgUrl = $originalInfo['headimgurl'];
+                $users->save();
+                $userInfo = UserInfo::where('openId',$openId)->first();
+                $request->session()->put('userId',$userInfo['userId']);
+                $request->session()->put('openId',$userInfo['openId']);
+                $request->session()->put('nickname',$userInfo['nickname']);
+                $request->session()->put('headimgurl',$userInfo['imgUrl']);
+                $request->session()->put('mobile',$userInfo['mobile']);
+            }
+        }
         return redirect($request->returnUrl);
     }
 }
