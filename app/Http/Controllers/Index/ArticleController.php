0485791de3e57e58bd2e6ca787f6470a8f5458f0
mafengxuan <mafengxuan@126.com>
2019-07-20 23:31:08 +0800

    ++

--- app/Http/Controllers/Index/ArticleController.php
+++ app/Http/Controllers/Index/ArticleController.php
@@ -102,7 +102,7 @@ class ArticleController extends Controller
     public function show($id)
     {
         //
-        $article = Article::where('id',$id)->with('user')->with('detail')->first();
+        $article = Article::where('id',$id)->with('user')->with('detail')->with('praise')->first();
 
         if(!empty($article)){
             if(!empty(session('userId'))){
