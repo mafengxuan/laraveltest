ad8a05bba75e4653181b1cceb2ccd0cb85cfcbda
mafengxuan <mafengxuan@126.com>
2019-08-16 16:48:38 +0800

    ++

--- app/Helpers/Tool.php
+++ app/Helpers/Tool.php
@@ -18,7 +18,9 @@ class Tool
             $name = $str;
             $name = preg_replace('/\xEE[\x80-\xBF][\x80-\xBF]|\xEF[\x81-\x83][\x80-\xBF]/', '', $name);
             $name = preg_replace('/xE0[x80-x9F][x80-xBF]‘.‘|xED[xA0-xBF][x80-xBF]/S','?', $name);
-            $return = json_decode(preg_replace("#(\\\ud[0-9a-f]{3})#ie","",json_encode($name)));
+            $return =  preg_replace_callback('/./u', function (array $match) {
+                return strlen($match[0]) >= 4 ? '' : $match[0];
+            }, $name);
             if(!$return){
                 return '';
             }
